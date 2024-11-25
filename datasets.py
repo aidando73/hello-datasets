3231d849920747892411bb8e37006a44fc825c2d
Aidan Do <aidand@canva.com>
2024-11-25 18:47:50 +1100

    .

--- datasets.py
+++
@@ -1,7 +0,0 @@
-import datasets
-
-# Load the dataset
-data = datasets.load_dataset("meta-llama/Llama-3.2-1B-Instruct-evals", name="Llama-3.2-1B-Instruct-evals")
-
-# Print the first 5 examples
-print(data["latest"][:5])
